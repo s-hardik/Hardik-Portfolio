ca6115194a90ba128969b4eed02c57ab3799a5a7
Shah <hardik.shah3@iqvia.com>
2022-08-25 19:04:16 +0530

    Fix: hero section fix

--- frontend_react/src/containers/Header/Header.jsx
+++ frontend_react/src/containers/Header/Header.jsx
@@ -22,9 +22,9 @@ const Header = () => {
       >
         <div className="app__header-badge">
           <div className="badge-cmp app__flex">
-            
+          <span>👋</span>
             <div style={{ marginleft: 20 }}>
-              <p className="p-text">Hello <span>👋</span>, I am</p>
+              <p className="p-text">Hello, I am</p>
               <h1 className="head-text">Hardik Shah</h1>
             </div>
           </div>
